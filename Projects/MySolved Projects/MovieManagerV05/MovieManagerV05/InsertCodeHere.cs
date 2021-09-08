b1d359ada9242bc3676c6e76fe6466f5984b9049
MitzDK <victorwilliamjejlskov@gmail.com>
2021-09-08 13:26:14 +0200

    InitialPush

---
+++ Projects/MySolved Projects/MovieManagerV05/MovieManagerV05/InsertCodeHere.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MovieManagerV05
+{
+    public class InsertCodeHere
+    {
+        string movieTitle;
+        int movieLength;
+        string movieMainActor;
+
+        public void MyCode()
+        {
+            Console.Clear();
+            // The FIRST line of code should be BELOW this line
+
+            Console.WriteLine("Please write the name of the movie!");
+            movieTitle = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("How long does the movie run for, in minutes?");
+            movieLength = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Who's the main actor/actress?");
+            movieMainActor = Console.ReadLine();
+            Console.Clear();
+
+            PrintMovie();
+            // The LAST line of code should be ABOVE this line
+        }
+        public void PrintMovie()
+        {
+            Console.WriteLine("The name of the movie is: " + movieTitle);
+            Console.WriteLine("And it runs for: " + movieLength + " minutes");
+            Console.WriteLine("The main actor/actress of the movie is: " + movieMainActor);
+        }
+    }
+}
---
+++ Projects/MySolved Projects/MovieManagerV05/MovieManagerV05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MovieManagerV05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertCodeHere theCode = new InsertCodeHere();
+            theCode.MyCode();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("If you wanna try again, click R");
+            Console.WriteLine();
+            Console.WriteLine("If not, press any other key to close the program...");
+
+            if(Console.ReadKey().Key == ConsoleKey.R)
+            {
+                theCode.MyCode();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
