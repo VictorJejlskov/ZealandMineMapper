e9e5bfd0c5d040565741242040c1711bdc80d65b
MitzDK <victorwilliamjejlskov@gmail.com>
2021-09-22 13:34:58 +0200

    FraSkole2.0

--- Projects/MySolved Projects/RolePlayV12/RolePlayV12/InsertCodeHere.cs
+++ Projects/MySolved Projects/RolePlayV12/RolePlayV12/InsertCodeHere.cs
@@ -7,27 +7,32 @@ namespace RolePlayV12
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
-            Sword swordA = new Sword("Meatcleaver", 100, 130);
+            Sword swordA = new Sword("Meatcleaver", 50, 100);
             Sword swordB = new Sword("Poke-Stick", 10, 300);
 
-            Warrior warriorA = new Warrior("Ragnar", 200, swordA);
-            Warrior warriorB = new Warrior("Lagertha", 240, swordB);
+            Warrior warriorA = new Warrior("Ragnar", 200, 1.7, swordA, swordA);
+            Warrior warriorB = new Warrior("Lagertha", 240, 1.3, swordB);
 
             Console.WriteLine("Just after creation:");
             Console.WriteLine(warriorA.GetInfo());
             Console.WriteLine(warriorB.GetInfo());
             Console.WriteLine();
 
-            int damageFromA = warriorA.DealDamage();
-            int damageFromB = warriorB.DealDamage();
-            warriorA.ReceiveDamage(damageFromB);
-            warriorB.ReceiveDamage(damageFromA);
+            double damageFromA = warriorA.DealDamage();
+            double damageFromB = warriorB.DealDamage();
+            warriorA.ReceiveDamage(Convert.ToInt32(damageFromB));
+            warriorB.ReceiveDamage(Convert.ToInt32(damageFromA));
 
             Console.WriteLine("After damage:");
             Console.WriteLine(warriorA.GetInfo());
             Console.WriteLine(warriorB.GetInfo());
             Console.WriteLine();
 
+            Console.WriteLine(warriorA.WeaponInfo());
+            Console.WriteLine(warriorB.WeaponInfo());
+
+            warriorB.SwapWeapon(swordA);
+            Console.WriteLine(warriorB.WeaponInfo());
             // The LAST line of code should be ABOVE this line
         }
     }
--- Projects/MySolved Projects/RolePlayV12/RolePlayV12/Warrior.cs
+++ Projects/MySolved Projects/RolePlayV12/RolePlayV12/Warrior.cs
@@ -9,17 +9,27 @@ namespace RolePlayV12
         private int _hitPoints;
         private int _maxDamage;
         private int _minDamage;
+        private double _strength;
         private Random _generator;
-        private Sword _sword;
+        private Sword _sword1;
+        private Sword _sword2;
         #endregion
 
         #region Constructor
-        public Warrior(string name, int hitPoints, Sword sword)
+        public Warrior(string name, int hitPoints, double strength, Sword sword1, Sword sword2)
         {
             _name = name;
             _hitPoints = hitPoints;
-            _sword = sword;
+            _sword1 = sword1;
+            _sword2 = sword2;
         }
+        public Warrior(string name, int hitPoints, double strength, Sword sword1)
+        {
+            _name = name;
+            _hitPoints = hitPoints;
+            _sword1 = sword1;
+        }
+
         #endregion
 
         #region Properties
@@ -37,6 +47,12 @@ namespace RolePlayV12
         {
             get { return _hitPoints <= 0; }
         }
+
+        public double Strength
+        {
+            get { return _strength; }
+            set { _strength = value; }
+        }
         #endregion
 
         #region Methods
@@ -45,15 +61,36 @@ namespace RolePlayV12
             _hitPoints = _hitPoints - points;
         }
 
-        public int DealDamage()
+        public double DealDamage()
         {
-            return _sword.DealDamage();
+            
+            if (_sword2 != null)
+            {
+                return (_sword1.DealDamage() + _sword2.DealDamage())*_strength;
+            }
+            return _sword1.DealDamage();
         }
 
         public string GetInfo()
         {
             return $"{Name} has {HitPoints} hit points ({(Dead ? "dead" : "alive")})";
         }
+
+        public string WeaponInfo()
+        {
+            if (_sword2 != null)
+            {
+                return $"{_name} is using {_sword1.Name} & {_sword2.Name}";
+            }
+
+            return $"{_name} is using {_sword1.Name}";
+        }
+
+        public void SwapWeapon(Sword sword)
+        {
+            _sword1 = sword;
+        }
+        
         #endregion
     }
 }
