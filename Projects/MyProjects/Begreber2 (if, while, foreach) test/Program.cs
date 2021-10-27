f7ed11afe9fac042e64c6f12af4a4b095a804039
MitzDK <victorwilliamjejlskov@gmail.com>
2021-10-27 09:40:32 +0200

    1

---
+++ Projects/MyProjects/Begreber2 (if, while, foreach) test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Begreber2__if__while__foreach__test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sandbox.RunCode();
+        }
+    }
+}
---
+++ Projects/MyProjects/Begreber2 (if, while, foreach) test/Sandbox.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Begreber2__if__while__foreach__test
+{
+    class Sandbox
+    {
+        public static void RunCode()
+        {
+            Console.WriteLine("Opg R.");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Tal: {i}");
+            }
+
+
+            Console.WriteLine($"\nOpg S");
+            for (int i = 2; i <= 10; i += 2)
+            {
+                Console.WriteLine($"Tal: {i}");
+            }
+
+            Console.WriteLine($"\nOpg T");
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"Tal: {i}");
+            }
+
+
+            Console.WriteLine($"\nOpg U");
+            int u = 1;
+            while (u <= 10)
+            {
+                Console.WriteLine($"Tal: {u}");
+                u++;
+            }
+
+            Console.WriteLine($"\nOpg V");
+            int v = 2;
+            while (v <= 10)
+            {
+                Console.WriteLine($"Tal: {v}");
+                v
+                    +=2;
+            }
+            Console.WriteLine($"\nOpg W");
+            int w = 10;
+            while (w >= 0)
+            {
+                Console.WriteLine($"Tal: {w}");
+                w--;
+            }
+
+            //Opg X - For løkker er bedst når du vil have at koden kører præcis den her mængde gange
+            //Opg Y - While løkker er gode når man vil køre et stykke kode indtil et kriterie er mødt, men ikke nødvendigvis ved hvor mange gang det køres før det sker.
+            //Opg Z - Foreach er bedst når man har med Lister at gøre, så kan man køre et stykke kode, for hver variabel i ens liste
+
+
+        }
+    }
+}
