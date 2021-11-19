bef4eebf281bf6a8a71370cad1233a0ae6e3f6c3
MitzDK <victorwilliamjejlskov@gmail.com>
2021-11-19 09:34:21 +0100

    lol

---
+++ Projects/RazorPageStuff/MySolved/Pizza_StoreV1/Pages/Pizzas/GetAllPizzas.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pizza_StoreV1.PizzaCatalogs;
+
+namespace Pizza_StoreV1
+{
+    public class GetAllPizzasModel : PageModel
+    {
+        private PizzaCatalog catalog;
+
+        public GetAllPizzasModel()
+        {
+            catalog = new PizzaCatalog();
+        }
+        public Dictionary<int, Models.Pizza> Pizzas { get; set; }
+
+        public IActionResult OnGet()
+        {
+            Pizzas = catalog.AllPizzas();
+            return Page();
+        }
+    }
+}
