75daddbc31e15e4d9d1e4aa781126db5065913f1
MitzDK <victorwilliamjejlskov@gmail.com>
2022-04-30 17:16:59 +0200

    123

---
+++ Projects/AndetSemester/ConsoleStuff/SandboxProject/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxProject
+{
+    class TestClass
+    {
+        public enum TestEnum
+        {
+            Eksempel1,
+            Eksempel2,
+            Eksempel3
+        }
+
+        public TestEnum MyEnum { get; set; }
+
+        public TestClass(TestEnum myEnum)
+        {
+            MyEnum = myEnum;
+        }
+    }
+}
---
+++ Projects/AndetSemester/ConsoleStuff/ThreadFun/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadFun
+{
+    class Test
+    {
+        public enum TestEnum
+        {
+            Eksempel1,
+            Eksempel2,
+            Eksempel3
+        }
+
+        public TestEnum MyEnum { get; set; }
+
+        public Test(TestEnum myEnum)
+        {
+            MyEnum = myEnum;
+        }
+    }
+}
