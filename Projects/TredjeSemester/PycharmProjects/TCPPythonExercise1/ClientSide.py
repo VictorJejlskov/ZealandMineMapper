2e7c94ceb4ae5b5854e0b2f6f4700c4ef2e5809a
MitzDK <victorwilliamjejlskov@gmail.com>
2022-10-06 12:10:00 +0200

    1

--- Projects/TredjeSemester/PycharmProjects/TCPPythonExercise1/ClientSide.py
+++ Projects/TredjeSemester/PycharmProjects/TCPPythonExercise1/ClientSide.py
@@ -1,13 +1,14 @@
 from socket import *
-serverName = '10.200.130.63'
+
+serverName = '10.200.130.71'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 while True:
-    sentence = input('Input lowercase sentence:')
+    sentence = input('Input: ')
     clientSocket.send(sentence.encode())
     modifiedSentence = clientSocket.recv(1024)
-    print('From Server: ', modifiedSentence.decode())
+    print(modifiedSentence.decode())
     if sentence == "exit":
         clientSocket.close()
         break
--- Projects/TredjeSemester/PycharmProjects/TCPPythonExercise1/ServerSide.py
+++ Projects/TredjeSemester/PycharmProjects/TCPPythonExercise1/ServerSide.py
@@ -1,3 +1,4 @@
+import threading
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -6,18 +7,38 @@ serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
 print("Server is hella ready")
-while True:
-    connectSocket, addr = serverSocket.accept()
+
+def handle_client(socket, address):
     while True:
-        sentence = connectSocket.recv(1024).decode()
-        print(sentence, addr)
-        capitalizedSentence = sentence.upper()
-        connectSocket.send(capitalizedSentence.encode())
-        if sentence.strip() == "exit":
-            print("connection closed")
-            connectSocket.close()
+        sentence = socket.recv(1024).decode()
+        print(sentence)
+        if " " in sentence:
+            command, words = sentence.split(" ")
+        else:
+            command = "NothingFound"
+
+        if command.lower() == "exit":
+            socket.send("Lukker forbindelsen :)".encode())
             break
-        if sentence.strip() == "":
-            print("connection closed")
-            connectSocket.close()
+        elif sentence.lower().strip() == "exit":
+            socket.send("Lukker forbindelsen :)".encode())
             break
+        elif command.lower() == "rvrs":
+            socket.send(words[::-1].encode())
+
+        elif command.lower() == "lwr":
+            socket.send(words.lower().encode())
+
+        elif command.lower() == "ppr":
+            socket.send(words.upper().encode())
+
+        elif command.lower() == "dd":
+            return_sentence = "Jeg er en glad lille cowboy, og du skrev: " + words
+            socket.send(return_sentence.encode())
+        else:
+            socket.send("Available commands are: 'exit', 'rvrs', 'lwr', 'ppr' and 'dd'".encode())
+    socket.close()
+
+while True:
+    connection_socket, addr = serverSocket.accept()
+    threading.Thread(target=handle_client, args=(connection_socket, addr)).start()
