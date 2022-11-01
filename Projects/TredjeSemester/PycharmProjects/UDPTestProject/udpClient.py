da9a85eccc868b21ce6d318e627c75ecff300a4c
MitzDK <victorwilliamjejlskov@gmail.com>
2022-11-01 12:10:42 +0100

    .

--- Projects/TredjeSemester/PycharmProjects/UDPTestProject/udpClient.py
+++ Projects/TredjeSemester/PycharmProjects/UDPTestProject/udpClient.py
@@ -3,7 +3,27 @@ serverName = '255.255.255.255'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-message = input('Input lowercase sentence:')
+
+
+def get_number():
+    choice = "wrong"
+    while not choice.isdigit():
+        choice = input("Input the speed, numbers only: ")
+
+        if not choice.isdigit():
+            print("Incorrect, numbers only.")
+        elif int(choice) >= 500:
+            print("Number has to be between 0 and 500")
+            choice = "wrong"
+        else:
+            return choice
+
+
+sensor_name = input("Input the name of the sensor: ")
+
+speed = get_number()
+
+message = sensor_name + " |-| " + speed
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
--- Projects/TredjeSemester/PycharmProjects/UDPTestProject/udpServer.py
+++ Projects/TredjeSemester/PycharmProjects/UDPTestProject/udpServer.py
@@ -14,9 +14,11 @@ serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 
 
-def post_speed_record():
+def post_speed_record(client_message):
     url = "https://localhost:7271/SpeedTraps"
-    data = {"SensorName": "VictorsSpeedTrap", "Speed": 80}
+
+    sensor_name, speed = client_message.split(" |-| ")
+    data = {"SensorName": sensor_name, "Speed": speed}
     response = requests.post(url, json=data, verify=False)
     print(response)
     print(response.json())
@@ -24,4 +26,5 @@ def post_speed_record():
 
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
-    post_speed_record()
+    print(message)
+    post_speed_record(message.decode())
