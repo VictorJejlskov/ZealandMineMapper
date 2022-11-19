de2a8840b751df53d5a52b6b58cb86e610e95a02
MitzDK <victorwilliamjejlskov@gmail.com>
2022-11-19 11:38:52 +0100

    .

--- Projects/TredjeSemester/MortenTimer/SpeedTrapREST/SpeedTrapREST/Program.cs
+++ Projects/TredjeSemester/MortenTimer/SpeedTrapREST/SpeedTrapREST/Program.cs
@@ -20,10 +20,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
+}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthorization();
