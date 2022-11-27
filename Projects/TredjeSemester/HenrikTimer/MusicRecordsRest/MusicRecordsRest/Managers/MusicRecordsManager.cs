e3afedaa5404e2f33421366bf849593e2b878204
MitzDK <victorwilliamjejlskov@gmail.com>
2022-11-27 14:55:10 +0100

    .

---
+++ Projects/TredjeSemester/HenrikTimer/MusicRecordsRest/MusicRecordsRest/Managers/MusicRecordsManager.cs
@@ -0,0 +1,34 @@
+﻿using MusicRecordsRest.Models;
+
+namespace MusicRecordsRest.Managers
+{
+    public class MusicRecordsManager
+    {
+        private static int _nextId = 1;
+        private List<MusicRecord> _records = new List<MusicRecord>()
+        {
+            new MusicRecord(){ Id = _nextId++, Title = "Gravity", Artist = "Boris Brejcha", Duration = 216, PublicationYear = 2019},
+            new MusicRecord(){ Id = _nextId++, Title = "Purple Noise", Artist = "Boris Brejcha", Duration = 539, PublicationYear = 2014},
+            new MusicRecord(){ Id = _nextId++, Title = "Up Down Jumper", Artist = "Boris Brejcha", Duration = 432, PublicationYear = 2022},
+            new MusicRecord(){ Id = _nextId++, Title = "House Music", Artist = "Boris Brejcha", Duration = 179, PublicationYear = 2021},
+            new MusicRecord(){ Id = _nextId++, Title = "Space Diver", Artist = "Boris Brejcha", Duration = 397, PublicationYear = 2020},
+        };
+
+        public List<MusicRecord> GetAll()
+        {
+            return _records;
+        }
+        public MusicRecord GetById(int id)
+        {
+            return _records.Find(record => record.Id == id);
+        }
+        public List<MusicRecord> GetByTitle(string searchParam)
+        {
+            return _records.FindAll(record => record.Title.ToLower().Contains(searchParam.ToLower()));
+        }
+        public List<MusicRecord> GetByArtist(string searchParam)
+        {
+            return _records.FindAll(record => record.Artist.ToLower().Contains(searchParam.ToLower()));
+        }
+    }
+}
---
+++ Projects/TredjeSemester/HenrikTimer/MusicRecordsRest/MusicRecordsRest/Models/MusicRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicRecordsRest.Models
+{
+    public class MusicRecord
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist{ get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+
+    }
+}
